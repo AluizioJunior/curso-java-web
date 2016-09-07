d529b34554351004f88a0949730f552466043e9f
Junior <soy_ninho@hotmail.com>
2016-09-07 04:59:09 -0300

    Implementação
    Criação da listagem no DAO e teste.

---
+++ fj21-jdbc/src/br/com/junior/jdbc/teste/TestaLista.java
@@ -0,0 +1,27 @@
+package br.com.junior.jdbc.teste;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+import br.com.junior.jdbc.dao.ContatoDao;
+import br.com.junior.jdbc.modelo.Contato;
+
+
+public class TestaLista {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		ContatoDao dao = new ContatoDao();
+		List<Contato> contatos = dao.getLista();
+		
+		for(Contato contato:contatos){
+			System.out.println("Nome:"+ contato.getNome());
+			System.out.println("Email:" + contato.getEmail());
+			System.out.println("Endereço:" + contato.getEndereco());
+			
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");			
+			System.out.println("Data de Nascimento:" + sdf.format(contato.getDataNascimento().getTime()) + "\n");
+			
+		}
+
+	}
+
+}
