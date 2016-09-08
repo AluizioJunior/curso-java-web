f55da2f97904a77572f99f1c453470a07839b797
Junior <soy_ninho@hotmail.com>
2016-09-07 23:18:40 -0300

    Implementação
    Criação do alterar e teste.

--- fj21-jdbc/src/br/com/junior/jdbc/dao/ContatoDao.java
+++ fj21-jdbc/src/br/com/junior/jdbc/dao/ContatoDao.java
@@ -75,4 +75,43 @@ public class ContatoDao {
 			throw new DAOException(e);
 		}
 	}
+	
+	//Altera um registro
+	public void altera(Contato contato){
+		String sql = "update contatos set nome=?, email=?, endereco=?, dataNascimento=? where id=?";
+		
+		try {
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setString(1, contato.getNome());
+			stmt.setString(2, contato.getEmail());
+			stmt.setString(3, contato.getEndereco());
+			stmt.setDate(4, new Date(contato.getDataNascimento().getTimeInMillis()));
+			stmt.setLong(5, contato.getId());
+			stmt.execute();
+			stmt.close();
+			System.out.println("Dados alterados com sucesso!!!");
+			
+		} catch (SQLException e) {
+			System.out.println("Não foi possível salvar as alterações!!!");
+			throw new RuntimeException(e);
+			
+		}
+	}
+	
+	//Remove um registro
+	public boolean remove(Contato contato){
+		try {
+			PreparedStatement stmt = connection.prepareStatement("delete"+
+		"from contatos where id?");
+			stmt.setLong(1, contato.getId());
+			stmt.execute();
+			stmt.close();
+			return true;
+		} catch (SQLException e) {
+			
+			//throw new RuntimeException(e);
+			return false;
+		}
+	}
+	
 }
